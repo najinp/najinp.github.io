c4ffc4f0b0ac4ed2c4d47998b14a3dd9b29fc6f5
najinp <105770758+najinp@users.noreply.github.com>
2022-07-14 17:54:05 +0900

    Add files via upload

--- abib/js/main.js
+++ abib/js/main.js
@@ -1,11 +1,32 @@
 $(function () {
 
+    $('.mainSlider').slick({
+        arrows: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+
+
+    $('.Bsslider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+        var calc = ((nextSlide) / (slick.slideCount - 1)) * 100;
+
+        $('.progress')
+            .css('background-size', calc + '% 100%')
+            .attr('aria-valuenow', calc);
+
+        $('.slider__label').text(calc + '% completed');
+    });
+
+
     $('.BsSlider').slick({
         slidesToShow: 3,
         arrows: false,
         pauseOnHover: false,
         pauseOnFocus: false,
+        draggable: true,
     });
+
 
 
 
