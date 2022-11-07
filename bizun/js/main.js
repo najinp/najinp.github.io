b4c537610148a70e0ae7956929bbb35e58458b30
najin <105770758+najinp@users.noreply.github.com>
2022-11-07 17:48:12 +0900

    Add files via upload

--- bizun/js/main.js
+++ bizun/js/main.js
@@ -1,42 +1,73 @@
+
 $(function () {
-  $('.main_slider').slick({
-    arrows: false,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  })
+    $('.main_slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 
-  $('.ProductSlider').slick({
-    arrows: false,
-    slidesToShow: 5,
-    responsive: [
-      {
-        breakpoint: 1230,
-        settings: {
-          slidesToShow: 1,
-          dots: true,
-        },
-      },
-    ],
-  })
+    $('.ProductSlider').slick({
+        arrows: false,
+        slidesToShow: 5,
+        responsive: [
+            {
+                breakpoint: 1230,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+            }]
 
-  function mopen() {
-    $(this).toggleClass('on')
-    $('.gnb').toggleClass('on')
-  }
+    });
+    $('.glslide').slick({
+        arrows: false,
+        slidesToShow: 1,
+    });
+    $('.grslide').slick({
+        arrows: false,
+        slidesToShow: 1,
+    });
 
-  $('.mopen').on('click', mopen)
+    function mopen() {
+        $(this).toggleClass('on');
+        $('.gnb').toggleClass('on');
+    }
+
+    $('.mopen').on('click', mopen)
+
+
+
+    $('.ingredientSlider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 1230,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+            }]
+    });
+
+
+    $('.ingredient .ingredientArrows i:nth-child(1)').on('click', function () {
+        $('.ingredientSlider').slick('slickPrev')
+    });
+    $('.ingredient .ingredientArrows i:nth-child(2)').on('click', function () {
+        $('.ingredientSlider').slick('slickNext')
+    });
+
+
+
 })
 
-$('.left_slider').slick({
-  arrows: false,
-  fade: true,
-  asNavFor: '.right_slider',
-})
-$('.right_slider').slick({
-  arrows: false,
-  asNavFor: '.left_slider',
-  slidesToShow: 3,
-  vertical: true,
-  verticalSwiping: true,
-})
+
+
+
+
+
+
